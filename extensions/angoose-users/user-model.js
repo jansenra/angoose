eb7815f733f8bbd23f833af12d2ccd0c4c64d423
Richard Jansen <humanications@gmail.com>
2015-07-13 19:15:53 +0200

    bugfix in password hashing

--- extensions/angoose-users/user-model.js
+++ extensions/angoose-users/user-model.js
@@ -1,4 +1,4 @@
-var angoose =require('../../lib/angoose');  //@TODO: should be able to use require("angoose") if the extension is individual module
+var angoose = require('../../lib/angoose');  //@TODO: should be able to use require("angoose") if the extension is individual module
 var bcrypt = require('bcrypt');
 var mongoose = angoose.getMongoose();
 var options = {
@@ -6,67 +6,68 @@ var options = {
     COLLECTION_NAME: 'angoose_users'
 }
 var UserSchema = new mongoose.Schema({
-                email: {type: String, required: true, label: 'Email', match:[/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i, "Email is not valid"], unique:true, tags:['default-list']},
-                name: {
-                    first: {type: String,   label: 'First Name' },
-                    middle: {type: String, label: 'Middle Name'},
-                    last: {type: String,   label: 'Last Name'  }
-                },
-                status: {
-                    type: String,
-                    required: true,
-                    label: 'Status',
-                    enum:['active','disabled','archived'],
-                    default:'active',
-                    tags:['default-list']
-                },
-                password: {type:String, required:true, label:'Password' },
-                roles:{type:String, enum:[ 'admin','manager','content-admin', 'user' ], default:'user', label:'User Role', required:true, tags:['default-list']} // the roles are sample ones
-        } , {collection: options.COLLECTION_NAME, label:'User'});
-        
-UserSchema.pre('save', function(next){
-    if(this.isModified('password')) {
-		// create salt and encrypt password
-		bcrypt.genSalt(10, function(err, salt) {
-			bcrypt.hash(this.password, salt, function(err, hash) {
-				this.password = hash;
-				next();
-			});
-		});
-    } else {
-		next();
-	}
-});
-UserSchema.methods.verifyPassword = function(password, callback ) {
-	bcrypt.compare(password, this.password, function(err, isMatch) {
-		if (err) {
-			return callback(err);
-		}
+    email: {type: String, required: true, label: 'Email', match: [/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i, "Email is not valid"], unique: true, tags: ['default-list']},
+    name: {
+        first: {type: String, label: 'First Name' },
+        middle: {type: String, label: 'Middle Name'},
+        last: {type: String, label: 'Last Name'  }
+    },
+    status: {
+        type: String,
+        required: true,
+        label: 'Status',
+        enum: ['active', 'disabled', 'archived'],
+        default: 'active',
+        tags: ['default-list']
+    },
+    password: {type: String, required: true, label: 'Password' },
+    roles: {type: String, enum: [ 'admin', 'manager', 'content-admin', 'user' ], default: 'user', label: 'User Role', required: true, tags: ['default-list']} // the roles are sample ones
+}, {collection: options.COLLECTION_NAME, label: 'User'});
 
-		callback(null, isMatch);
-	});
+UserSchema.pre('save', function (next) {
+    var self = this;
+    if (this.isModified('password')) {
+        // create salt and encrypt password
+        bcrypt.genSalt(10, function (err, salt) {
+            bcrypt.hash(self.password, salt, function (err, hash) {
+                self.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+UserSchema.methods.verifyPassword = function (password, callback) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+        if (err) {
+            return callback(err);
+        }
+
+        callback(null, isMatch);
+    });
 };
-module.exports =  mongoose.model( options.MODEL_NAME, UserSchema);
+module.exports = mongoose.model(options.MODEL_NAME, UserSchema);
 
 
 // create a default user
 
 var adminUser = {
-    email:'admin@demo.com',
-    password:'demo',
+    email: 'admin@demo.com',
+    password: 'demo',
     name: {
-        first:'Admin', last:'Demo'
+        first: 'Admin', last: 'Demo'
     },
-    status:'active',
-    roles:'admin'
+    status: 'active',
+    roles: 'admin'
 }
-setTimeout(function(){
-    var UserModel=  mongoose.model(options.MODEL_NAME);
-    UserModel.findOne({email: adminUser.email}, function(err, user){
-        if(user) return;
+setTimeout(function () {
+    var UserModel = mongoose.model(options.MODEL_NAME);
+    UserModel.findOne({email: adminUser.email}, function (err, user) {
+        if (user) return;
         var u = new UserModel(adminUser);
-        u.save(function(err){
+        u.save(function (err) {
             console.log("Add default admin user result:", err);
         });
     })
-},1000);
+}, 1000);
